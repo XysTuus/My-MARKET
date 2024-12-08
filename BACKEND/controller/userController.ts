3f7339178965e8dd67032ccd16e33ba9fcbc7dae
Sixtus1-oss <ogbonnasixtus6@gmail.com>
2024-12-08 20:37:15 +0100

    test commit

--- BACKEND/controller/userController.ts
+++ BACKEND/controller/userController.ts
@@ -90,3 +90,50 @@ export const loginAccount = async (
     });
   }
 };
+
+export const verifyAccount = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    try {
+      const { email, password } = req.body;
+      const user = await userModel.findById({ email });
+      if (user) {
+        const check = await bcrypt.compare(password, user?.password);
+        if (check) {
+          if (user?.isVerified && user?.verifiedToken === "") {
+            const token = jwt.sign(
+              { id: user?.id },
+              process.env.JWT_SECRET as string,
+              { expiresIn: process.env.JWT_TIMEOUT as string }
+            );
+            return res.status(201).json({
+              message: "Login successful",
+              data: user,
+              status: 201,
+            });
+          } else {
+            return res.status(404).json({
+              message: "please verify your account",
+              statu: 404,
+            });
+          }
+        } else {
+          return res.status(404).json({
+            message: "incorrect password",
+            status: 404,
+          });
+        }
+      } else {
+        return res.status(404).json({
+          message: "user not found",
+          status: 404,
+        });
+      }
+    } catch (error) {
+      return res.status(404).json({
+        message: "Error encountered while logging in",
+        status: 404,
+      });
+    }
+  };
